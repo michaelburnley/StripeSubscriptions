b7304a2064755bcaa6de0fc280ebd419b6325cac
Michael Burnley <michael@cybercowgames.com>
2016-01-12 16:59:26 -0800

    Fix broken .gitignore

--- subscription.rb
+++ subscription.rb
@@ -10,41 +10,40 @@ require 'csv'
 config = YAML.load_file('config.yml')
 client_url = 'https://api.stripe.com'
 Stripe.api_key = config[:API_KEY]
-###### End Basic Config Information #####
-
-# Stripe::Customer.create(
-#   :description => "Customer for test@example.com",
-#   :source => "tok_16RRpRC5mprcFNjzVmOcX2nl" # obtained with Stripe.js
-# )
-
-stripeCustomer = Stripe::Customer.all(:limit => 3)
+stripeCustomer = Stripe::Customer.all #(:limit => 3)
+stripePlans = Stripe::Plan.all #(:limit => 3)
 ALLCUSTOMERS = stripeCustomer['data']
-
-def createCustomer()
-end
-
-def exportAllCustomers()
-	File.open("export.csv", 'w')
-
-end
-
-def updateCustomer(body)
-end
-
-# def findCustomer(email)
-# 	selectedCustomer = ALLCUSTOMERS.select {|x| x['email'] == email}
-# 	customerID = selectedCustomer[0]['id']
-# 	return customerID
-# end
+ALLPLANS = stripePlans['data']
+###### End Basic Config Information #####
 
 def findCustomerID(criteria, value)
 	selectedCustomer = ALLCUSTOMERS.select {|x| x["#{criteria}"] == value}
 	return selectedCustomer[0]['id']
 end
 
+def createCustomer(email, coupon, plan)
+	#Stripe::Customer.create(
+	#	:email =>	email
+	#	:coupon =>	coupon
+	#	:plan =>	plan
+	#	)
+end
+
+def createSubscription(customerID, planID)
+	cu = Stripe::Customer.retrieve(customerID)
+	cu.subscriptions.create({:plan => planID})
+	cu.save
+end
+
 selectCustomer = findCustomerID("email","test@test.com")
+puts ALLPLANS[0]
 puts selectCustomer
 
+
+# Stripe::Customer.create(
+#   :description => "Customer for test@example.com",
+#   :source => "tok_16RRpRC5mprcFNjzVmOcX2nl" # obtained with Stripe.js
+# )
 
 # customerID = allCustomers['data'][0]['id']
 # cu = Stripe::Customer.retrieve(customerID)
